ef7d3f96bc91ef13421c6ccbccf45cbedbf02fbd
Vinayak Pandey <Vinayakpandey286@gmail.com>
2023-03-19 14:49:55 +0530

    Front-End

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+        <Routes>
+          <Route exact path="/" element={<HomeScreen/>}> </Route>
+          <Route path="/product/:id" element={<ProductScreen/>}> </Route>
+        </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 };
 
